e3cf4548882bc7403935472a3243d54fa10b7f61
bartsev <mikhail.bartsev@travelline.ru>
2020-01-29 10:52:01 +0300

    Написал методы для работы с резюме

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,72 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
 
-    void clear() {
+    private Resume[] storage = new Resume[10000];
+    private int storageSize;
+
+    public void clear() {
+        Arrays.fill(storage, 0, storageSize, null);
     }
 
-    void save(Resume r) {
+    public void save(Resume resume) {
+        if (storageSize < storage.length) {
+            storage[storageSize] = resume;
+            storageSize++;
+        } else {
+            System.out.println("Массив переполнен и новая запись невозможна");
+        }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
+        Integer index = getIndexByIdentifier(uuid);
+        if (index != null) {
+            return storage[index];
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        Integer index = getIndexByIdentifier(uuid);
+        if (index != null) {
+            storageSize--;
+            storage[index] = storage[storageSize];
+            storage[storageSize] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, storageSize);
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return storage.length;
+    }
+
+    private Integer getIndexByIdentifier(String uuid) {
+        int numberOfResumes = 0;
+        int index = 0;
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].getIdentifier().equals(uuid)) {
+                numberOfResumes++;
+                index = i;
+            }
+        }
+        if (numberOfResumes == 1) {
+            return index;
+        } else {
+            if (numberOfResumes == 0) {
+                System.out.println("Не найдено резюме с идентификатором " + uuid);
+            } else {
+                System.out.println("Найдено несколько резюме с идентификатором " + uuid);
+            }
+        }
+        return null;
     }
 }
